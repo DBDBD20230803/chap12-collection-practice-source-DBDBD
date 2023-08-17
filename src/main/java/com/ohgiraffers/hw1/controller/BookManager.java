b174e1ef3ada519f0f6ce7db24d33025f37b2361
peaknicc <ybkz17@naver.com>
2023-08-17 17:49:56 +0900

    init commit

---
+++ src/main/java/com/ohgiraffers/hw1/controller/BookManager.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.hw1.controller;
+
+import com.ohgiraffers.hw1.model.dto.BookDTO;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookManager {
+    private ArrayList<BookDTO> booklist;
+    private Scanner sc;
+
+    public BookManager() {
+
+    }
+
+    public void addBook(Book book) {
+
+    }
+
+    public void deleteBook(int index) {
+
+    }
+
+    public void searchBook(String btitle) {
+
+    }
+
+    public void displayAll() {
+
+    }
+
+    public List<BookDTO> sortedBookList(int type) {
+        return null;
+    }
+
+    public void printBookList(List<BookDTO> printList) {
+
+    }
+}
---
+++ src/main/java/com/ohgiraffers/hw1/view/BookMenu.java
@@ -0,0 +1,36 @@
+package com.ohgiraffers.hw1.view;
+
+import com.ohgiraffers.hw1.controller.BookManager;
+import com.ohgiraffers.hw1.model.dto.BookDTO;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class BookMenu {
+    private Scanner sc;
+    private BookManager bm;
+
+    public BookMenu() {
+
+    }
+
+    public void mainMenu() {
+
+    }
+
+    public BookDTO inputBook() {
+        return null;
+    }
+
+    public String inputBookNo() {
+        return null;
+    }
+
+    public String inputBookTitle() {
+        return null;
+    }
+
+    public List<BookDTO> selectSortedBook() {
+        return null;
+    }
+}
